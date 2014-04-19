1b7185b5c8917f6545e8e5d0c656f0372035e3e6
= <=>
2014-04-20 01:11:29 +0800

    retrieve queryString

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ var url = require("url");
 var start = function(route, handler) {
 	var onRequest = function(request, response) {
 		var pathname = url.parse(request.url).pathname;
+                var queryString = url.parse(request.url).query;
 		console.log("request is received, url is " + pathname);
 		
 		response.writeHead(200, {"Content-type": "text/plain"});
 		response.write(route(handler, pathname));
 		response.end();
 
-		console.log("request processing is done, url is " + pathname);	
+		console.log("request processing is done, url is " + pathname 
+                    + ", queryString is " + queryString);
 	};
 
 	http.createServer(onRequest).listen(8080);
